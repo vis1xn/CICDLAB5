50dd073fd640378af4c0108878ebd21b70a2df57
vis1xn <g00421432@atu.ie>
2024-10-22 09:53:47 +0100

    Started lab 5 code

---
+++ src/main/java/ie/atu/week6lab5/Week6Lab5Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week6lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6Lab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6Lab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week6lab5/Week6Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week6lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week6Lab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
