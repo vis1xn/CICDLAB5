3ebee1111acb521fd2373d2f954710b7de5395b0
vis1xn <g00421432@atu.ie>
2024-10-22 11:49:04 +0100

    added error validation, update, and delete

--- src/main/java/ie/atu/week6lab5/ProductController.java
+++ src/main/java/ie/atu/week6lab5/ProductController.java
@@ -1,9 +1,7 @@
 package ie.atu.week6lab5;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +11,27 @@ public class ProductController {
     private ProductService myService;
 
     public ProductController(ProductService myService) {
+
         this.myService = myService;
     }
 
     private List<Product> list = new ArrayList<>();
     //Creating a class which is interested in requests and responses. Separation of Concern
-    @PostMapping
+    @PostMapping("/create")
     public List<Product> newProduct(@RequestBody Product product){
         list = myService.addProduct(product);
         return list;
     }
+    @PutMapping("/update/{id}")
+    public List<Product> updateProduct(@PathVariable int id, @RequestBody Product product){
+        list = myService.updateProduct(id, product);
+        return list;
+    }
+
+    @DeleteMapping("/update/{id}")
+    public List<Product> deleteProduct(@PathVariable int id, @RequestBody Product product){
+        list = myService.deleteProduct(id, product);
+        return list;
+    }
+
 }
--- src/main/java/ie/atu/week6lab5/ProductService.java
+++ src/main/java/ie/atu/week6lab5/ProductService.java
@@ -9,9 +9,26 @@ public class ProductService {
     private List<Product> myList  = new ArrayList<>();
 
     public List<Product> addProduct(Product product){
-        //Do business stuff like retrieving from db, or generating files in pdfs
         myList.add(product);
         return myList;
 
     }
+    public List<Product> updateProduct( int id, Product product) {
+        for (Product p : myList) {
+            if (product.getId() == id) {
+                myList.remove(p);
+            }
+        }
+        myList.add(product);
+        return myList;
+    }
+    public List<Product> deleteProduct( int id, Product product) {
+        for (Product p : myList) {
+            if (product.getId() == id) {
+                myList.remove(p);
+            }
+        }
+        return myList;
+    }
+
 }
